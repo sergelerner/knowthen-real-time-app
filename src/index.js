5c9c154e0c06dc4accd7122fe9175a62cdc641ec
Serge Lerner <serge@yodas.com>
2018-04-01 09:09:18 +0530

    Env setup with Webpack 4 Babel and React

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = document.createElement('root');
+document.body.appendChild(root);
+
+ReactDOM.render(
+  <h1>hello world!!! :)</h1>,
+  root
+);
